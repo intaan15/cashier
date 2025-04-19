cf9ce3bdbad7dd48d21bace993adb4a14812e3ce
Intan Lailatul Ilmiana <intaanlailatul@gmail.com>
2025-04-19 18:27:53 +0700

    update menu

--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function menu() {
   return (
-    <div>menu</div>
+    <div className='container'>
+      <div className='bg-green-700'>
+        ini nanti isinya menu
+      </div>
+    </div>
   )
 }
 
